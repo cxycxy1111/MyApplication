e3edaf6009f804facaa119fee629f5ca440cfa89
cxycxy11 <cxycxy1111@gmail.com>
2017-11-01 10:19:04 +0800

    11月1日新增

--- app/src/main/java/com/example/dengweixiong/Adapter/SupportedCardAdapter.java
+++ app/src/main/java/com/example/dengweixiong/Adapter/SupportedCardAdapter.java
@@ -21,24 +21,43 @@ public class SupportedCardAdapter extends RecyclerView.Adapter implements View.O
 
     private OnItemClickListener listener;
     private ArrayList<HashMap<String,String>> mapList;
-    private Course course;
+    private static final int CARD_TYPE_BALANCE = 1;
+    private static final int CARD_TYPE_TIMES = 2;
+    private static final int CARD_TYPE_TIME = 3;
 
-    public SupportedCardAdapter(Course course) {
-        this.course = course;
+    public SupportedCardAdapter(ArrayList<HashMap<String,String>> mapList) {
+        this.mapList = mapList;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class BalanceVH extends RecyclerView.ViewHolder {
         CheckBox checkBox;
         EditText editText_balance;
-        EditText editText_num;
-        public ViewHolder(View view) {
+        public BalanceVH(View view) {
             super(view);
             checkBox = (CheckBox)view.findViewById(R.id.cb_t_balance_card);
             editText_balance = (EditText) view.findViewById(R.id.et_t_balance_card);
-            editText_num = (EditText)view.findViewById(R.id.et_t_balance_card);
         }
-
     }
+    
+    public static class TimesVH extends RecyclerView.ViewHolder {
+        CheckBox checkBox;
+        EditText editText;
+        public TimesVH(View view) {
+            super(view);
+            checkBox = (CheckBox)view.findViewById(R.id.cb_t_times_card);
+            editText = (EditText) view.findViewById(R.id.et_t_times_card);
+        }
+    }
+
+    public static class TimeVH extends RecyclerView.ViewHolder {
+        CheckBox checkBox;
+        EditText editText;
+        public TimeVH(View view) {
+            super(view);
+            checkBox = (CheckBox)view.findViewById(R.id.cb_t_time_card);
+        }
+    }
+
 
     public interface OnItemClickListener {
         void onItemClick(View view,int position);
@@ -58,32 +77,35 @@ public class SupportedCardAdapter extends RecyclerView.Adapter implements View.O
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tile_supportedcard_balance,parent,false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        view.setOnClickListener(this);
-        return null;
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        switch (viewType) {
+            case CARD_TYPE_BALANCE :
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tile_supportedcard_balance,parent,false);
+                return new BalanceVH(view);
+            case CARD_TYPE_TIMES:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tile_supportedcard_times,parent,false);
+                return new TimesVH(view);
+            case CARD_TYPE_TIME :
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tile_supportedcard_time,parent,false);
+                return new TimeVH(view);
+            default:
+                return null;
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        String cardName = course.getSupportedCard().get(position).getName();
         HashMap<String,String> map = mapList.get(position);
-        ViewHolder holder1 =  (ViewHolder)holder;
-        holder1.checkBox.setText(cardName);
-
-        if(course.getSupportedCard().get(position).getType() == 1) {
-            int price = course.getPrice().get(position).getBalance();
-            holder1.editText_balance.setText(String.valueOf(price));
-            holder1.editText_num.setVisibility(View.GONE);
-        } else if (course.getSupportedCard().get(position).getType() == 2) {
-            holder1.editText_num.setText(course.getPrice().get(position).getBalance());
-            holder1.editText_balance.setVisibility(View.GONE);
+        switch (holder.getItemViewType()) {
+            case CARD_TYPE_BALANCE:
+                BalanceVH balanceVH = (BalanceVH)holder;
+                balanceVH.checkBox.setText(map.get(""));
         }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mapList.size();
     }
 }
